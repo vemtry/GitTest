fa099663f49b0e155cb78ca23b2bdd912a290ac9
Your Name <jamesraj1990@gmail.com>
2018-01-06 14:16:58 +0530

    src commit

---
+++ src/main/java/gittest/Gittest.java
@@ -0,0 +1,12 @@
+package gittest;
+
+public class Gittest {
+	@Test
+	public void loginTest(){
+		Webdriver wd=new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com");
+		File src=(Take screenshot)wd.
+	}
+
+}
